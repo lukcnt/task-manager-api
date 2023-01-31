b36d584850e3c2aa5b51ebfa5f2813ed6f77052a
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 23:14:23 -0300

    Add the services builder for the connection string

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using task_manager_api.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<OrganizerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
