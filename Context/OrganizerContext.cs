7b4b9c1e57c8a6bd06179bf8ecb696f437d51185
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 23:09:40 -0300

    Create the dbcontext for the project

---
+++ Context/OrganizerContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using task_manager_api.Models;
+
+namespace task_manager_api.Context
+{
+    public class OrganizerContext : DbContext
+    {
+        public OrganizerContext(DbContextOptions<OrganizerContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
